4fdb550e7858899360bf0cbcc5ff60f2f03601bc
Suresh Kumar <suresh.alagarswamy@oracle.com>
2024-08-10 16:41:00 -0400

    Updated files

--- RSALib.s
+++ RSALib.s
@@ -11,6 +11,9 @@
 .global encrypt
 .global decrypt
 .global modulus_exponentiation
+.global encrypt_message
+.global decrypt_message
+.global print_line_separator
 
 .text
 checkRange:
@@ -377,9 +380,6 @@ cpubexp:
 	CMP r0, r1
 
 	BNE ExpErrorCoPrime
-	    LDR r0, =validExpMsg
-	    MOV r1, r10
-	    BL printf
  	    MOV r0, r10
 	    B EndCPubExp
 
@@ -586,6 +586,7 @@ decrypt:
 .data
 
 #END decrypt
+# ----------------------------------------------------------------------------------
 
 .text
 modulus_exponentiation:
@@ -656,4 +657,337 @@ modulus_exponentiation:
 
 #END encrypt
 
+# ----------------------------------------------------------------------------------
+# Purpose: To encrypt a clear text file
+#   We will use encrypt functionn to encrypt contents in clear text file.
+#   
+#   Input : public key exponent (r0), modulus N (r1)
+#   Output: None
+
+.text
+encrypt_message:
+
+    # Program dictionary
+    # r4 - public key exponent
+    # r5 - modulus N
+
+    # Push to the stack
+    SUB sp, sp, #12
+    STR lr, [sp]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+
+    # Store input pubKeyExp into R4
+    MOV r4, r0
+    # Store input modulus N into R5
+    MOV r5, r1
+
+    BL print_line_separator
+
+    LDR r0, =promptFile
+    BL printf
+
+    LDR r0, =inputFormat
+    LDR r1, =input_file
+    BL scanf
+
+    LDR r0, =input_file
+    LDR r1, =file_read_mode
+    BL fopen
+
+    LDR r1, =file_read_pointer
+    STR r0, [r1]
+    CMP r0, #0
+    BEQ invalid_file
+
+        LDR r0, =outputFileFormat
+        LDR r1, =input_file
+        BL printf
+
+        # Initialize write file pointer
+        LDR r0, =output_file_name
+        LDR r1, =file_write_mode
+        BL fopen
+        LDR r1, =file_write_pointer
+        STR r0, [r1]
+
+    read_loop:
+        LDR r0, =file_read_pointer
+        LDR r0, [r0]
+        BL fgetc
+        CMP r0, #-1
+        BEQ end_read_loop
+
+            LDR r1, =file_content
+            STR r0, [r1]
+
+            LDR r1, =file_content
+            LDR r1, [r1]
+            MOV r0, r1
+            MOV r1, r4
+            MOV r2, r5
+
+            # Process file content character by character
+            # Logic to encrypt character goes here
+            BL modulus_exponentiation
+            MOV r2, r0
+
+            LDR r0, =file_write_pointer
+            LDR r0, [r0]
+            LDR r1, =writeFileContentFormat
+            BL fprintf
+
+            LDR r1, =file_content
+            LDR r1, [r1]
+            LDR r0, =outputFileContentFormat
+            BL printf
+
+            B read_loop
+        
+    end_read_loop:
+        B close_file
+
+    close_file:
+        LDR r0, =file_read_pointer
+        LDR r0, [r0]
+        BL fclose
+        LDR r0, =file_write_pointer
+        LDR r0, [r0]
+        BL fclose
+        B done
+
+    invalid_file:
+        LDR r0, =errorInvalidFile
+        BL printf
+
+    done:
+
+    LDR r0, =outputNextLineFormat
+    BL printf
+
+    LDR r0, =outputEncryptedFileFormat
+    BL printf
+
+    # Pop from stack
+    LDR lr, [sp]
+    LDR r4, [sp, #4]
+    LDR r5, [sp, #8]
+    ADD sp, sp, #12
+    MOV pc, lr
+
+.data
+    promptFile: .asciz "\nEnter a clear-text file path to encrypt: "
+    inputFormat: .asciz "%s"
+    input_file: .space 50
+
+    outputFileFormat: .asciz "\nContent of the file [ %s ] is: "
+    outputFileContentFormat: .asciz "%c"
+    outputEncryptedFileFormat: .asciz "Encrypted content is written to file [ 'encrypted.txt' ]\n"
+    outputNextLineFormat: .asciz "\n"
+
+    writeFileContentFormat: .asciz "%d "
+
+    errorInvalidFile: .asciz "\nError: File doesn't exist or access denied\n"
+    file_content: .word 0
+
+    file_read_pointer: .word 0
+    file_write_pointer: .word 0
+    file_read_mode: .asciz  "r"
+    file_write_mode: .asciz  "w"
+    output_file_name: .asciz "encrypted.txt"
+
+# END encrypt_message
+# ----------------------------------------------------------------------------------
+# Purpose: To decrypt an encrypted file
+#   We will use decrypt functionn to decrypt contents in encrypted file.
+#   
+#   Input : private key exponent (r0), modulus N (r1)
+#   Output: None
+
+.text
+decrypt_message:
+
+    # Program dictionary
+    # r4 - private key exponent
+    # r5 - modulus N
+
+    # Push to the stack
+    SUB sp, sp, #12
+    STR lr, [sp]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+
+    # Store input privKeyExp into R4
+    MOV r4, r0
+    # Store input modulus N into R5
+    MOV r5, r1
+
+    BL print_line_separator
+
+    LDR r0, =promptDecryptFile
+    BL printf
+
+    LDR r0, =inputDecryptFormat
+    LDR r1, =decrypt_input_file
+    BL scanf
+
+    LDR r0, =decrypt_input_file
+    LDR r1, =decrypt_file_read_mode
+    BL fopen
+
+    LDR r1, =decrypt_file_read_pointer
+    STR r0, [r1]
+    CMP r0, #0
+    BEQ decrypt_invalid_file
+
+        LDR r0, =decrypt_outputFileFormat
+        LDR r1, =decrypt_input_file
+        BL printf
+
+        # Initialize write file pointer
+        LDR r0, =decrypt_output_file_name
+        LDR r1, =decrypt_file_write_mode
+        BL fopen
+        LDR r1, =decrypt_file_write_pointer
+        STR r0, [r1]
+
+    decryption_read_loop:
+        LDR r0, =decrypt_file_read_pointer
+        LDR r0, [r0]
+        LDR r1, =decrypt_readFileContentFormat
+        LDR r2, =encrypted_file_content
+        BL fscanf
+
+        CMP r0, #1
+        BNE end_decryption_read_loop
+
+            LDR r0, =encrypted_file_content
+            LDR r0, [r0]
+
+            MOV r1, r4
+            MOV r2, r5
+
+            # Process file content character by character
+            # Logic to encrypt character goes here
+            BL modulus_exponentiation
+            MOV r2, r0
+
+            LDR r1, =decrypt_file_content
+            STR r0, [r1]
+
+            LDR r0, =decrypt_file_write_pointer
+            LDR r0, [r0]
+            LDR r1, =decrypt_writeFileContentFormat
+            BL fprintf
+
+            LDR r1, =decrypt_file_content
+            LDR r1, [r1]
+            LDR r0, =decrypt_outputFileContentFormat
+            BL printf
+
+            B decryption_read_loop
+        
+    end_decryption_read_loop:
+        B decrypt_close_file
+
+    decrypt_close_file:
+        LDR r0, =decrypt_file_read_pointer
+        LDR r0, [r0]
+        BL fclose
+        LDR r0, =decrypt_file_write_pointer
+        LDR r0, [r0]
+        BL fclose
+        B decrypt_done
+
+    decrypt_invalid_file:
+        LDR r0, =decrypt_errorInvalidFile
+        BL printf
+
+    decrypt_done:
+
+    LDR r0, =decrypt_outputNextLineFormat
+    BL printf
+
+    LDR r0, =decrypt_outputDecryptedFileFormat
+    BL printf
+
+    BL print_line_separator
+
+    # Pop from stack
+    LDR lr, [sp]
+    LDR r4, [sp, #4]
+    LDR r5, [sp, #8]
+    ADD sp, sp, #12
+    MOV pc, lr
+
+.data
+    promptDecryptFile: .asciz "\nEnter a encrypted file path to decrypt: "
+    inputDecryptFormat: .asciz "%s"
+    decrypt_input_file: .space 50
+
+    decrypt_outputFileFormat: .asciz "\nContent of the file [ %s ] is: "
+    decrypt_readFileContentFormat: .asciz "%d"
+    decrypt_outputFileContentFormat: .asciz "%d "
+    decrypt_outputDecryptedFileFormat: .asciz "\nDecrypted content is written to file [ 'decrypted.txt' ]\n"
+    decrypt_outputNextLineFormat: .asciz "\n"
+
+    decrypt_writeFileContentFormat: .asciz "%c"
+
+    decrypt_errorInvalidFile: .asciz "\nError: File doesn't exist or access denied\n"
+    decrypt_file_content: .space 40
+    encrypted_file_content: .word 0
+
+    decrypt_file_read_pointer: .word 0
+    decrypt_file_write_pointer: .word 0
+    decrypt_file_read_mode: .asciz  "r"
+    decrypt_file_write_mode: .asciz  "w"
+    decrypt_output_file_name: .asciz "decrypted.txt"
+
+# END decrypt_message
+# ----------------------------------------------------------------------------------
+.text
+print_line_separator:
+    # Push to the stack
+    SUB sp, sp, #4
+    STR lr, [sp, #0]
+
+    LDR r1, =loop_counter
+    LDR r1, [r1]
+
+    LDR r0, =outputNextLine
+    BL printf
+
+    startLoop:
+        CMP r1, #80
+        BGT endLoop
+            LDR r0, =outputLineSeparator
+            BL printf
+
+            LDR r1, =loop_counter
+            LDR r1, [r1]
+            ADD r1, r1, #1
+            LDR r3, =loop_counter
+            STR r1, [r3]
+            B startLoop
+    endLoop:
+
+    LDR r0, =outputNextLine
+    BL printf
+
+    MOV r1, #1
+    LDR r3, =loop_counter
+    STR r1, [r3]
+
+    # Pop from stack and return
+    LDR lr, [sp, #0]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    outputLineSeparator: .asciz "-"
+    outputNextLine: .asciz "\n"
+    loop_counter: .word 1
+
+# END main 
+# ----------------------------------------------------------------------------------
 
